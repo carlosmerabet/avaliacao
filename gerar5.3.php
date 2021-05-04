fa70cef9e1aeaa848619d3d82d9214410b5f34b3
carlosmerabet <52975004+carlosmerabet@users.noreply.github.com>
2021-05-04 17:11:26 -0300

    Add files via upload

--- gerar5.3.php
+++ gerar5.3.php
@@ -23,38 +23,34 @@ $horaInicio = date('d-m-Y H-i-s');
 
 foreach($res as $v){
     $texto = utf8_encode($v['ra_conteudo']);
-    if( strpos( $texto , "4ª Vara da Família e Sucessões")){
-        
-        
-        $pos = stripos($texto, "JUIZ(A) DE DIREITO ");       
-        $juiz = substr($texto, $pos+19, 100); 
-        $vet = explode(" ",$juiz);
-        $string = "";
-        for($i=0; $i <= count($vet); $i++ ){
-            if(stripos($vet[$i], "ESCRIVÃ(O)")){
-                $o = explode("\n", $vet[$i]);
-                $string .= $o[0]." ";
-                break;
-            }
-            $string .= $vet[$i]." ";
-           
+
+
+    $pos = stripos($texto, "JUIZ(A) DE DIREITO ");       
+    $juiz = substr($texto, $pos+19, 100); 
+    $vet = explode(" ",$juiz);
+    $string = "";
+    for($i=0; $i <= count($vet); $i++ ){
+        if(stripos($vet[$i], "ESCRIVÃ(O)")){
+            $o = explode("\n", $vet[$i]);
+            $string .= $o[0]." ";
+            break;
         }
-
-
-        $juiz = trim($string);
-        $pos = stripos($texto, "Processo ");
-        $processo = substr($texto, $pos+9, 25);
-
-        $vetor = [
-            "processo"  =>  $processo,
-            "juiz"      =>  $juiz,
-            "conteudo"  =>  $texto
-        ];
-
-        $vetorGeral[]=$vetor;
-
-
+        $string .= $vet[$i]." ";
+       
     }
+
+
+    $juiz = trim($string);
+    $pos = stripos($texto, "Processo ");
+    $processo = substr($texto, $pos+9, 25);
+
+    $vetor = [
+        "processo"  =>  $processo,
+        "juiz"      =>  $juiz,
+        "conteudo"  =>  $texto
+    ];
+
+    $vetorGeral[]=$vetor;    
 
 }
 
